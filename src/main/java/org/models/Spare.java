939c88b60a706fe9974b60e081eb7351236e8c0e
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 17:52:46 -0500

    feat(models/Spare.java): defining Spare attributes

--- src/main/java/org/models/Spare.java
+++ src/main/java/org/models/Spare.java
@@ -1,4 +1,13 @@
 package org.models;
 
 public class Spare {
+    
+    private Long id;
+    private String name;
+    private String reference;
+    private Integer category;
+    private Double price;
+    private Integer stock;
+    private boolean active = true;
+    
 }
