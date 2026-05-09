b4ab05180f67954947a5806f7da31589904efca0
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 17:52:15 -0500

    feat(models/sell.java): defining sell attributes

--- src/main/java/org/models/Sell.java
+++ src/main/java/org/models/Sell.java
@@ -1,4 +1,16 @@
 package org.models;
 
+import java.time.LocalDateTime;
+
 public class Sell {
+
+    private Long id;
+    private LocalDateTime date;
+    private String clientName;
+    private String productName;
+    private Integer quantity;
+    private Double unitaryPrice;
+    private Double total;
+    private String registeredBy;
+    
 }
