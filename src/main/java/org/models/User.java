d2cfb80cc9efc5abe349beaad89d0d3ce596b208
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 17:51:34 -0500

    feat(models/user.java): entity attributes

--- src/main/java/org/models/User.java
+++ src/main/java/org/models/User.java
@@ -1,4 +1,12 @@
 package org.models;
+import org.models.enums.Rol;
 
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Rol rol;
+    private boolean active = true;
 }
