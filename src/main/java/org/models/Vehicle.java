51cfb2c9587582a5f3040ec9ac862118408d6d41
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 17:53:22 -0500

    feat(models/vehicle): defining Vehicle attributes

--- src/main/java/org/models/Vehicle.java
+++ src/main/java/org/models/Vehicle.java
@@ -1,4 +1,13 @@
 package org.models;
 
 public class Vehicle {
+
+    private Long id;
+    private String brand;
+    private String model;
+    private Integer year;
+    private Double price;
+    private Integer sock;
+    private boolean active = true;
+    
 }
