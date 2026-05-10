1a236f757e617b01f877e8ddc57423e284cc0ef1
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 21:29:15 -0500

    feat(config/ApplicationConfig): enable scheduling for NotificationService

--- src/main/java/org/config/ApplicationConfig.java
+++ src/main/java/org/config/ApplicationConfig.java
@@ -1,4 +1,11 @@
 package org.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling  // Activates @Scheduled in NotificationService
 public class ApplicationConfig {
+
 }
