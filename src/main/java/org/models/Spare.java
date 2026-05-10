8d643064855bf0ea5306b449add45a6fbdbfa0e8
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 20:06:58 -0500

    fix(models/Spare): changing datatype that hasn't anything related with category as variable

--- src/main/java/org/models/Spare.java
+++ src/main/java/org/models/Spare.java
@@ -5,7 +5,7 @@ public class Spare {
     private Long id;
     private String name;
     private String reference;
-    private Integer category;
+    private String category;
     private Double price;
     private Integer stock;
     private boolean active = true;
