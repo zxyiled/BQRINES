43d5f89b88db8f65ef854e8a7bf5a6b669d3f2f5
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 21:30:28 -0500

    feat(exception/StockInsuficienteException): rename and extend RuntimeException for stock validation

--- src/main/java/org/exception/InsufficientStockException.java
+++ src/main/java/org/exception/InsufficientStockException.java
@@ -1,4 +1,8 @@
 package org.exception;
 
-public class InsufficientStockException {
+public class StockInsuficienteException extends RuntimeException {
+
+    public StockInsuficienteException(String message) {
+        super(message);
+    }
 }
