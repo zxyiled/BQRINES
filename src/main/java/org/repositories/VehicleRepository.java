200a06a795bc53e6c8c73e0a9c78ebada042d050
zxyiled <sebastianmejiadev@gmail.com>
2026-05-09 20:56:56 -0500

    feat(repositories/VehicleRepository): convert to Spring Data JPA repository interface

--- src/main/java/org/repositories/VehicleRepository.java
+++ src/main/java/org/repositories/VehicleRepository.java
@@ -1,9 +1,13 @@
 package org.repositories;
 
 import org.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-public class VehicleRepository {
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     // Returns only active vehicles for catalog display
     List<Vehicle> findByActiveTrue();
